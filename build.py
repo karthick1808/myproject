086f3c2a7eb6b86b8bba10f2f17de772a9d85a5c
Karthick1808 <karthickspikey@gmail.com>
2023-11-19 17:53:43 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,14 +1,17 @@
-from pybuilder.core import use_plugin, init, task
+#   -*- coding: utf-8 -*-
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
-use_plugin("python.pytest")
 use_plugin("python.flake8")
-use_plugin("python.pylint")
-use_plugin("python.bandit")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
 
+
+name = "myproject"
 default_task = "publish"
+
 
 @init
 def set_properties(project):
-    project.build_depends_on('prometheus_client', 'requests')
+    pass
